62b812537ae1e3b088d1c1369a46d04bf4ee8711
SuhailMoideen07 <suhailmoideen08@gmail.com>
2025-12-06 15:25:50 +0530

    hero section updated

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
+import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
   <div className="mt-40">
-  <Button variant={"destructive"}>suhail</Button>
+    <HeroSection /> 
   </div>
   )
 }
--- components/header.jsx
+++ components/header.jsx
@@ -10,44 +10,44 @@ const Header = () => {
     <div className="fixed top-0 w-full bg-white/90 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
-          <Image 
-            src={"/logo.webp"} 
-            alt="Finova Logo" 
-            height={60} 
+          <Image
+            src={"/logo.webp"}
+            alt="Finova Logo"
+            height={60}
             width={200}
-            className="h-12 w-auto object-contain"/>
-          
-        </Link>
-      <div className="flex items-center space-x-4">
-        <SignedIn>
-          <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600,flex items-center gap-2">
-          <Button variant="outline">
-            <LayoutDashboard size={18}/>
-            <span className="hidden md:inline">Dashboard</span>
-          </Button>
-          </Link>
-        <Link href="/transaction/create">
-          <Button  className="flex items-center gap-2">
-            <PenBox size={18}/>
-            <span className="hidden md:inline">Add Transaction</span>
-          </Button>
-          </Link>
-        </SignedIn>
-         <SignedOut>
-         <SignInButton forceRedirectUrl="/dashboard">
-          <Button variant="outline">Login</Button>
-        </SignInButton>
-      </SignedOut>
+            className="h-12 w-auto object-contain" />
 
-      <SignedIn>
-        <UserButton appearance={{
-          elements:{
-            avatarBox: "w-10 h-10",
-          },
-        }} />
-      </SignedIn>
-      </div>
-    </nav>
+        </Link>
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600,flex items-center gap-2">
+              <Button variant="outline">
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">Dashboard</span>
+              </Button>
+            </Link>
+            <Link href="/transaction/create">
+              <Button className="flex items-center gap-2">
+                <PenBox size={18} />
+                <span className="hidden md:inline">Add Transaction</span>
+              </Button>
+            </Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton appearance={{
+              elements: {
+                avatarBox: "w-10 h-10",
+              },
+            }} />
+          </SignedIn>
+        </div>
+      </nav>
     </div>
   );
 };
