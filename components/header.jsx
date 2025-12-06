9d54aeef91f0a872e46b9912ac098a30659d1646
SuhailMoideen07 <suhailmoideen08@gmail.com>
2025-12-06 10:24:18 +0530

    authentication setup completed

--- components/header.jsx
+++ components/header.jsx
@@ -4,6 +4,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Layout, LayoutDashboard, PenBox } from "lucide-react";
 const Header = () => {
   return (
     <div className="fixed top-0 w-full bg-white/90 backdrop-blur-md z-50 border-b">
@@ -17,15 +18,33 @@ const Header = () => {
             className="h-12 w-auto object-contain"/>
           
         </Link>
-      <div>
-      <SignedOut>
-        <SignInButton forceRedirectUrl="/dashboard">
+      <div className="flex items-center space-x-4">
+        <SignedIn>
+          <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600,flex items-center gap-2">
+          <Button variant="outline">
+            <LayoutDashboard size={18}/>
+            <span className="hidden md:inline">Dashboard</span>
+          </Button>
+          </Link>
+        <Link href="/transaction/create">
+          <Button  className="flex items-center gap-2">
+            <PenBox size={18}/>
+            <span className="hidden md:inline">Add Transaction</span>
+          </Button>
+          </Link>
+        </SignedIn>
+         <SignedOut>
+         <SignInButton forceRedirectUrl="/dashboard">
           <Button variant="outline">Login</Button>
         </SignInButton>
       </SignedOut>
 
       <SignedIn>
-        <UserButton />
+        <UserButton appearance={{
+          elements:{
+            avatarBox: "w-10 h-10",
+          },
+        }} />
       </SignedIn>
       </div>
     </nav>
