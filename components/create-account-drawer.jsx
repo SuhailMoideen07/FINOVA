9ad0d1110cd8a5638bd5b4236335d244ce4950c2
SuhailMoideen07 <suhailmoideen08@gmail.com>
2025-12-15 20:23:09 +0530

    account drawer setup

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -8,8 +8,29 @@ import {
   DrawerTitle,
   DrawerTrigger
 } from './ui/drawer'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from './ui/input'
+import { accountSchema } from '@/app/lib/schema'
 const CreateAccountDrawer = ({ children }) => {
   const [open, setOpen] = useState(false)
+  const { register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+    reset,
+  } =
+    useForm({
+      resolver: zodResolver(accountSchema),
+      defaultValues: {
+        name: '',
+        type: 'CURRENT',
+        balance: '',
+        isDefault: false,
+      },
+    }
+    )
 
   return (
     <Drawer open={open} onOpenChange={setOpen}>
@@ -18,8 +39,30 @@ const CreateAccountDrawer = ({ children }) => {
         <DrawerHeader>
           <DrawerTitle>Create New Account</DrawerTitle>
         </DrawerHeader>
-        <div>
-          <form ></form>
+        <div className='px-4 pb-4'>
+          <form className='space-y-4' >
+            <div className='space-y-2'>
+              <label htmlFor="name" className='text-sm font-medium'>Account Name</label>
+              <Input
+                id="name"
+                placeholder="e.g., Main Checking"
+                {...register('name')} />
+              {errors.name && (<p className='text-sm text-red-500'>{errors.name.message}</p>)}
+            </div>
+            <div className='space-y-2'>
+              <label htmlFor="type" className='text-sm font-medium'>Account Type</label>
+              <Select onValueChange={(value) => setValue('type', value)}>
+                <SelectTrigger id="type">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="CURRENT">Current</SelectItem>
+                  <SelectItem value="SAVINGS">Savings</SelectItem>
+                </SelectContent>
+              </Select>
+              {errors.name && (<p className='text-sm text-red-500'>{errors.name.message}</p>)}
+            </div>
+          </form>
         </div>
       </DrawerContent>
     </Drawer>
