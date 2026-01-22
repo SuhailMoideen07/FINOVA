0a4d6860ea086d14922d9680b6dce60e01fa541a
SuhailMoideen07 <suhailmoideen08@gmail.com>
2026-01-22 12:46:10 +0530

    middle ware bundle size reduced

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,7 +2,7 @@ import { inngest } from "@/lib/inngest/client";
 import {  checkBudgetAlerts, generateMonthlyReports } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 import { processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
-
+import { protectRoute } from "@/lib/arcjet";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -13,3 +13,9 @@ export const { GET, POST, PUT } = serve({
     generateMonthlyReports,
   ],
 });
+export async function POST(req) {
+  const protection = await protectRoute(req);
+  if (protection) return protection;
+  
+  // Your inngest code...
+}
--- app/api/seed/route.js
+++ app/api/seed/route.js
@@ -1,6 +1,11 @@
 import { seedTransactions } from "@/actions/seed";
+import { protectRoute } from "@/lib/arcjet";
 
-export async function GET() {
+export async function GET(req) {
+  // Add Arcjet protection
+  const protection = await protectRoute(req);
+  if (protection) return protection;
+
   const result = await seedTransactions();
   return Response.json(result);
 }
